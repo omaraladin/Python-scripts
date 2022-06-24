961f6749f2e002bdb5615d3a5657491d9d53ee68
Omar Aladdin <48786827+omaraladin@users.noreply.github.com>
2022-06-24 10:39:26 +0400

    Lights will guide you home

--- api.py
+++ api.py
@@ -3,9 +3,10 @@
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
 import pandas as pd
-import astapp = Flask(__name__)
+import ast
 import os
 
+app = Flask(__name__28)
 api = Api(app)
 
 class Users(Resource):
